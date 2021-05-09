2962cc4ebec275230d0b8a7ea69651137916f59c
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-05-09 06:44:18 +0200

    Add button for celcus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,85 @@
-import { getWeatherData } from './weather/temp';
+import { renderInfo } from './weather/temp';
 
-getWeatherData();
+const inputEl = document.getElementById('city-name');
+const searchBtn = document.getElementById('search-btn');
+const metricBtn = document.querySelector('.metric');
+const imperialBtn = document.querySelector('.imperial');
+
+// Select only the needed information
+function filterWeatherData(data) {
+  return {
+    id: data.weather[0].id,
+    name: data.name,
+    description: data.weather[0].description,
+    icon: data.weather[0].icon,
+    temp: Math.round(data.main.temp),
+    feels: Math.round(data.main.feels_like),
+    humidity: data.main.humidity,
+    wind: data.wind.speed,
+    pressure: data.main.pressure,
+  };
+}
+
+// Fetch information
+function fetchWeather(name, unit) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=${unit}&appid=06390dd87d5264ce0e550a12e2f79b20`,
+    {
+      mode: 'cors',
+    },
+  )
+    .then((resp) => resp.json())
+    .then((resp) => {
+      if (resp.cod && resp.cod === '404') {
+        throw Error(resp.message);
+      }
+      return resp;
+    })
+    .then((resp) => {
+      const selectedCityWeather = filterWeatherData(resp);
+      renderInfo(selectedCityWeather, unit);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+
+// Add functionality to buttons
+const setWeather = ((e) => {
+  let unit = 'metric';
+  let name = 'new york';
+
+  const search = (e) => {
+    if (inputEl.value) {
+      e.preventDefault();
+      name = inputEl.value;
+      fetchWeather(name, unit);
+    } else alert('Put a valid city name');
+  };
+
+  const switchToMetric = () => {
+    unit = 'metric';
+    fetchWeather(name, unit);
+  };
+
+  const switchToImperial = () => {
+    unit = 'imperial';
+    fetchWeather(name, unit);
+  };
+
+  return {
+    search,
+    switchToMetric,
+    switchToImperial,
+  };
+})();
+
+// Initialize
+fetchWeather('new york', 'metric');
+
+searchBtn.addEventListener('click', (e) => {
+  setWeather.search(e);
+});
+
+imperialBtn.addEventListener('click', setWeather.switchToImperial);
+metricBtn.addEventListener('click', setWeather.switchToMetric);
--- src/weather/temp.js
+++ src/weather/temp.js
@@ -1,131 +1,40 @@
-let searches = [];
-let showError = true;
+const nameEl = document.querySelector('.city-name');
+const descriptionEl = document.querySelector('.weather-description');
+const iconEl = document.querySelector('.icon');
+const temperatureEl = document.querySelector('.temperature');
+const windEl = document.querySelector('.wind-data');
+const pressureEl = document.querySelector('.pressure-data');
+const feelEl = document.querySelector('.feel-data');
+const humidityEl = document.querySelector('.humidity-data');
+const metricBtn = document.querySelector('.metric');
+const imperialBtn = document.querySelector('.imperial');
 
-const content = document.querySelector('#content');
-const divMain = document.createElement('div');
-divMain.setAttribute('class', 'mainDiv');
-const pageTitle = document.createElement('h1');
-pageTitle.setAttribute('class', 'app-title');
-pageTitle.textContent = 'Weather App';
-divMain.appendChild(pageTitle);
-content.appendChild(divMain);
-const searchButton = document.getElementById('searchbutton');
-const searchInput = document.getElementById('searchinput');
-const createError = (message) => {
-  if (showError) {
-    showError = false;
-    const search = document.getElementById('search');
-    const paragraph = document.createElement('p');
-    paragraph.id = 'error';
-    paragraph.textContent = `Oops, an error occured: ${message}`;
-    search.appendChild(paragraph);
-    setTimeout(() => {
-      paragraph.classList.toggle('fade');
-    }, 2000);
-    setTimeout(() => {
-      search.removeChild(paragraph);
-      showError = true;
-    }, 2200);
+const renderInfo = (data, unit) => {
+  iconEl.src = `https://openweathermap.org/img/wn/${data.icon}@2x.png`;
+  nameEl.textContent = data.name;
+  descriptionEl.textContent = data.description;
+  humidityEl.textContent = `${data.humidity} %`;
+  humidityEl.style.fontWeight = '200';
+  pressureEl.textContent = `${data.pressure} hPa`;
+  pressureEl.style.fontWeight = '200';
+
+  if (unit === 'metric') {
+    temperatureEl.textContent = `${data.temp} ${String.fromCharCode(176)}C`;
+    windEl.textContent = `${data.humidity} km/h`;
+    windEl.style.fontWeight = '200';
+    feelEl.textContent = `${data.feels} ${String.fromCharCode(176)}C`;
+    feelEl.style.fontWeight = '200';
+    metricBtn.classList.add('active');
+    imperialBtn.classList.remove('active');
+  } else {
+    temperatureEl.textContent = `${data.temp} ${String.fromCharCode(176)}F`;
+    windEl.textContent = `${data.humidity} m/h`;
+    windEl.style.fontWeight = '200';
+    feelEl.textContent = `${data.feels} ${String.fromCharCode(176)}F`;
+    feelEl.style.fontWeight = '200';
+    metricBtn.classList.remove('active');
+    imperialBtn.classList.add('active');
   }
 };
 
-const render = () => {
-  const container = document.getElementById('history');
-  container.textContent = '';
-  searches.forEach((search) => {
-    const main = document.createElement('div');
-    const innerDiv = document.createElement('div');
-    main.id = 'data-search';
-    main.setAttribute('class', 'main-container');
-    const location = document.createElement('h4');
-    main.appendChild(innerDiv);
-    innerDiv.appendChild(location);
-    location.textContent = search.location;
-    location.id = 'location';
-    const datadiv = document.createElement('div');
-    datadiv.id = 'data';
-    const description = document.createElement('p');
-    description.textContent = search.description.charAt(0).toUpperCase()
-     + search.description.slice(1);
-    description.id = 'description';
-    datadiv.appendChild(description);
-    const temp = document.createElement('p');
-    temp.textContent = `Temperature: ${search.temp}`;
-    temp.id = 'temp';
-    datadiv.appendChild(temp);
-    const feelsLike = document.createElement('p');
-    feelsLike.textContent = `Feels like: ${search.feels_like}`;
-    feelsLike.id = 'feels_like';
-    datadiv.appendChild(feelsLike);
-    const humidity = document.createElement('p');
-    humidity.textContent = `Humidity: ${search.humidity}`;
-    humidity.id = 'humidity';
-    datadiv.appendChild(humidity);
-    const windSpeed = document.createElement('p');
-    windSpeed.textContent = `Wind speed: ${search.wind_speed}`;
-    windSpeed.id = 'wind_speed';
-    datadiv.appendChild(windSpeed);
-    innerDiv.appendChild(datadiv);
-    const icon = document.createElement('img');
-    icon.id = 'icon';
-    container.appendChild(main);
-    main.classList.toggle('fade');
-    setTimeout(() => {
-      main.classList.toggle('fade');
-    }, 200);
-  });
-  searchButton.disabled = false;
-};
-
-const api = {
-  key: '&appid=7eda76c00c8d93b65261bc92f2f405e0',
-  url: 'https://api.openweathermap.org/data/2.5/weather?q=',
-};
-
-const convertToF = (celsius) => {
-  const fahrenheit = celsius(9 / 5) + 32;
-  return fahrenheit;
-};
-
-const processData = (json) => {
-  const data = {
-    location: `${json.name}, ${json.sys.country}`,
-    temp: `${Math.round(json.main.temp) - 273} °C`,
-    feels_like: `${Math.round(json.main.feels_like) - 273} °C`,
-    humidity: `${json.main.humidity} %`,
-    wind_speed: `${Math.round(json.wind.speed)} MPH`,
-    description: `${json.weather[0].description}`,
-    icon: `${json.weather[0].icon}`,
-  };
-  searches = [data, ...searches];
-  render();
-};
-
-const tempSwitch = document.getElementById('my-switch');
-
-tempSwitch.addEventListener('click', () => {
-  if (processData.data.temp) {
-    return `${(processData.data.temp * 1.8) + 32} °F`;
-  }
-});
-
-const getWeatherData = (location) => {
-  searchButton.disabled = true;
-
-  fetch(`${api.url}${location}${api.key}`, { mode: 'cors' })
-    .then(async (response) => {
-      const json = await response.json();
-      if (!json.message) {
-        processData(json);
-      } else {
-        createError(json.message);
-        searchButton.disabled = false;
-      }
-    });
-};
-
-searchButton.addEventListener('click', () => {
-  getWeatherData(searchInput.value);
-});
-
-export { getWeatherData };
+export { renderInfo };
